1024dc06ebf516cac3a7e2fe7b1fcaa3e5a00ce0
lucio <colombolucio@hotmail.com>
2021-02-17 11:54:15 -0300

    minor fixes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
   
   useEffect(()=>{
     localStorage.setItem("tasks",JSON.stringify(infoArray))
+    const kk=JSON.parse(localStorage.getItem("tasks"))
+    let summm=0
+      for(let x=0;x<kk.length;x++){
+      summm=kk[x]["amount"]*1+summm
+      setTotal(summm)
+      }
   })
 
   const btnHandler= function({amount, description, date}){
@@ -23,29 +29,36 @@ function App() {
    const clearNow=function(){
    window.location.reload(false);
     localStorage.clear()
+    
    }
  
   return (
-    <div>
+  <div>
     <div className="App mx-5 border ">
       <InputBar btnHandler={btnHandler} clearNow={clearNow}/>
-        <div className=" d-flex ">
-          <div className="border comp">
-            <h3 className=" border"> Description</h3>
-            <div >{infoArray.map((x)=><li className="ml-4">{x.description}  </li> )}</div>
-          </div>
-          <div className="border comp">
-            <h3 className="border">Amount </h3>
-            <div >{infoArray.map((x)=><li className="ml-4">  <NumberFormat value={x.amount} displayType={'text'} thousandSeparator={true} prefix={'$'} /></li> )}</div> 
-          </div>
-          <div className="border comp">
-            <h3 className="border">Date </h3>
-            <div>{infoArray.map((x)=><li className="ml-4">{x.dateString}  </li> )}</div>
-          </div>
-          </div>
-        </div>
-        <h3 className="col-9 border text-right mt-3 ml-5 ">Total <NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={'$'} /></h3>
-      </div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{infoArray.map((x)=><li className="ml-4 text-decoration-none">{infoArray.indexOf(x)+1}  </li> )}</th>
+            <td>{infoArray.map((x)=><li className="ml-4 list-unstyled">{x.description}  </li> )}</td>
+            <td>{infoArray.map((x)=><li className="ml-4 list-unstyled">  <NumberFormat value={x.amount} displayType={'text'} thousandSeparator={true} prefix={'$'} /></li> )}</td>
+            <td>{infoArray.map((x)=><li className="ml-4 list-unstyled">{x.dateString}  </li> )}</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+    <h3 className="col-11 border text-right mt-3 ml-5 ">Total <NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={'$'} /></h3>
+    <footer>This App was created by Lucio Colombo. It uses local storage to remember saved costs.</footer>
+  </div>
     
     
   );
--- src/InputBar.js
+++ src/InputBar.js
@@ -18,14 +18,23 @@ function InputBar({btnHandler , clearNow  }) {
     return (
         <div  className="my-4 mx-2 ">
             <form  onSubmit={e => e.preventDefault()}>
-            <label className="mr-1" htmlFor="description">Description </label>
-            <input className="mr-3" id="description" value={description} onChange={e=>setDescription(e.target.value)} placeholder="Description" required></input>
-            <label className="mr-1" htmlFor="amount">Amount $ </label>
-            <input className="mr-3" id="amount" value={amount} onChange={e=>setAmount(e.target.value)} placeholder="Amount" required></input>
-            <label className="mr-1" htmlFor="date">Date </label>
-            <DatePicker className="mr-3" id="date" onChange={setDate} value={date}/>
-            <button onClick={()=>{onClick1()}}>Add</button>
-            <button onClick={()=>{clearNow()}}>Clear</button> 
+                    <section className="description"> 
+                    <label className="mr-1 ml-1" htmlFor="description">Description </label>
+                    <input className="mr-3" id="description" value={description} onChange={e=>setDescription(e.target.value)} placeholder="Description" required></input>
+                    </section>
+                    <section className="amount">
+                    <label className="mr-1" htmlFor="amount">Amount $ </label>
+                    <input className="mb-1" id="amount" value={amount} onChange={e=>setAmount(e.target.value)} placeholder="Amount" required></input>
+                    </section>
+                    <section className="date">
+                    <label className="mr-1" htmlFor="date">Date </label>
+                    <DatePicker className="mr-3 ml-2" id="date" onChange={setDate} value={date}/>
+                    </section>
+                <div className="w-100 ml-1 mt-1 text-center">
+                    <button className="btn btn-primary w-25 mr-1 mt-2 btnadd" onClick={()=>{onClick1()}}>Add</button>
+                    <button className="btn btn-danger mt-2" onClick={()=>{clearNow()}}>Clear</button> 
+                </div>
+                
             </form>     
             
         </div>
