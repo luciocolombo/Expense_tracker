7697d7fb216f0346c14d91147bbae788001f3fac
lucio <colombolucio@hotmail.com>
2021-03-01 10:40:41 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -5,43 +5,51 @@ import NumberFormat from 'react-number-format';
 import RegisterForm from "./RegisterForm"
 import LoginForm from "./LoginForm"
 import Dashboard from "./Dashboard"
-
+import axios from 'axios'
 
 function App() {
  
   
-
   const localData =localStorage.getItem("tasks")
   const [infoArray, setInfoArray]=useState(localData? JSON.parse(localData):[])
   const [total, setTotal]=useState(0)
   var id=1
   
-/*   LOCAL STORAGE VERSION */
+
     useEffect(()=>{
     localStorage.setItem("tasks",JSON.stringify(infoArray))
     const kk=JSON.parse(localStorage.getItem("tasks"))
     let summm=0
+    //aca hago la suma del total
       for(let x=0;x<kk.length;x++){
       summm=kk[x]["amount"]*1+summm
       setTotal(summm)
       }
-  })
+  },[infoArray])
 
-  //FULLSTACK VERSION (debo agregar el historial de gastos en la DB en vez de local storage)
-  /*   useEffect(()=>{
-      axios.get('http://localhost:4000/posts',)
-    }) */
+     useEffect(()=>
+      {
+      const user=localStorage.getItem("user")
+      axios.patch(`http://localhost:4000/posts/${user}`,{expenses:localStorage.getItem("taks")});
+      }
+      ,[infoArray]  //Ver setTimer y cantidad de pedidos a axios. Crear ruta PATCH
+     
+    ) 
 
   const btnHandler= function({amount, description, date}){
     var id=id+1
     const dateString=date.toString().slice(3,16)
     setInfoArray([...infoArray, {amount, description, dateString}])
     setTotal(total*1+amount*1)
+    localStorage.setItem("tasks",infoArray)
+    localStorage.setItem("total",total)
     
   }
    const clearNow=function(){
    window.location.reload(false);
-    /* localStorage.clear()  VERSON LOCAL STORAGE*/
+    localStorage.clear("tasks")
+    localStorage.clear("total")
+
     //ACA HAGO AXIOS.DELETE
   /*   que interprete USER como variable. Luegoe de patchear debe actualizar las cookies.get(data) */
     
@@ -82,7 +90,7 @@ function App() {
           
             </div>
             <h3 className="col-11 border text-right mt-3 ml-5 ">Total <NumberFormat value={total} displayType={'text'} thousandSeparator={true} prefix={'$'} /></h3>
-            <footer>This App was created by Lucio Colombo. It uses local storage to remember saved costs.</footer>
+            <footer>This App was created by Lucio Colombo. It lets you save your data expenses for better money management</footer>
           </div>
       </Route>
     </Router>
